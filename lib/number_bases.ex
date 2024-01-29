9d306f2336d0cbd4f03cfa1884fa1928353c5ea3
Kerry Buckley <kerryjbuckley@gmail.com>
2024-01-29 21:29:48 +0000

    Extract parse and format functions

--- lib/number_bases.ex
+++ lib/number_bases.ex
@@ -1,5 +1,13 @@
 defmodule NumberBases do
   def convert_number(number, input_base, output_base) do
-    number |> String.to_integer(input_base) |> Integer.to_string(output_base)
+    number |> parse(input_base) |> format(output_base)
+  end
+
+  defp parse(number, base) do
+    String.to_integer(number, base)
+  end
+
+  defp format(number, base) do
+    Integer.to_string(number, base)
   end
 end
