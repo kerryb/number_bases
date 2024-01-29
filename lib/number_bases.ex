045a0334a675147be351f1de24d535f98b0a0d55
Kerry Buckley <kerryjbuckley@gmail.com>
2024-01-29 22:40:51 +0000

    Validate inputs

--- lib/number_bases.ex
+++ lib/number_bases.ex
@@ -1,16 +1,27 @@
 defmodule NumberBases do
   def convert_number(number, input_base, output_base) do
+    validate_base(input_base, "input")
+    validate_base(output_base, "output")
     number |> parse(input_base) |> format(output_base)
   end
+
+  defp validate_base(base, label) when base not in 2..36, do: raise("Invalid #{label} base")
+  defp validate_base(_base, _label), do: :ok
 
   defp parse(number, base) do
     number
     |> String.to_charlist()
-    |> Enum.reduce(0, fn digit, acc -> acc * base + parse_digit(digit) end)
+    |> Enum.reduce(0, fn digit, acc ->
+      case parse_digit(digit) do
+        n when n >= base -> raise("Invalid number for base #{base}")
+        n -> acc * base + n
+      end
+    end)
   end
 
   defp parse_digit(char) when char in ?A..?Z, do: char - 55
   defp parse_digit(char) when char in ?0..?9, do: char - ?0
+  defp parse_digit(_char), do: raise("Invalid number")
 
   defp format(0, _base), do: ""
 
--- test/number_bases_test.exs
+++ test/number_bases_test.exs
@@ -2,14 +2,52 @@ defmodule NumberBasesTest do
   use ExUnit.Case
   use PropCheck
 
-  property "Matches built-in language implementation" do
-    forall {number, input_base, output_base} <- {resize(integer(), 1_000_000), base(), base()} do
-      input_string = Integer.to_string(number, input_base)
+  describe "NumberBases.convert_number/3" do
+    property "Matches built-in language implementation" do
+      forall {number, input_base, output_base} <- {resize(integer(), 1_000_000), base(), base()} do
+        input_string = Integer.to_string(number, input_base)
 
-      NumberBases.convert_number(input_string, input_base, output_base) ==
-        Integer.to_string(number, output_base)
+        NumberBases.convert_number(input_string, input_base, output_base) ==
+          Integer.to_string(number, output_base)
+      end
+    end
+
+    defp base, do: range(2, 36)
+
+    test "raises an exception for completely invalid input" do
+      assert_raise(RuntimeError, "Invalid number", fn ->
+        NumberBases.convert_number("?!💩", 10, 16)
+      end)
+    end
+
+    test "raises an exception for input that’s invalid for the base" do
+      assert_raise(RuntimeError, "Invalid number for base 10", fn ->
+        NumberBases.convert_number("A", 10, 16)
+      end)
+    end
+
+    test "raises an exception for an input base less than 2" do
+      assert_raise(RuntimeError, "Invalid input base", fn ->
+        NumberBases.convert_number("0", 1, 10)
+      end)
+    end
+
+    test "raises an exception for an input base more than 36" do
+      assert_raise(RuntimeError, "Invalid input base", fn ->
+        NumberBases.convert_number("0", 37, 10)
+      end)
+    end
+
+    test "raises an exception for an output base less than 2" do
+      assert_raise(RuntimeError, "Invalid output base", fn ->
+        NumberBases.convert_number("0", 10, 1)
+      end)
+    end
+
+    test "raises an exception for an output base more than 36" do
+      assert_raise(RuntimeError, "Invalid output base", fn ->
+        NumberBases.convert_number("0", 10, 37)
+      end)
     end
   end
-
-  defp base, do: range(2, 36)
 end
