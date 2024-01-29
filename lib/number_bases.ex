63221808dbc02d141063caa76f1749fa962aea42
Kerry Buckley <kerryjbuckley@gmail.com>
2024-01-29 22:20:01 +0000

    Implement format ourselves

--- lib/number_bases.ex
+++ lib/number_bases.ex
@@ -12,7 +12,12 @@ defmodule NumberBases do
   defp parse_digit(char) when char in ?A..?Z, do: char - 55
   defp parse_digit(char) when char in ?0..?9, do: char - ?0
 
+  defp format(0, _base), do: ""
+
   defp format(number, base) do
-    Integer.to_string(number, base)
+    format(div(number, base), base) <> format_digit(Integer.mod(number, base))
   end
+
+  defp format_digit(digit) when digit < 10, do: to_string(digit)
+  defp format_digit(digit), do: <<digit + 55>>
 end
