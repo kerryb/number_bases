d494ca467054efbe996113cf9cf1c07045f321f6
Kerry Buckley <kerryjbuckley@gmail.com>
2024-01-29 21:58:14 +0000

    Implement parse ourselves

--- lib/number_bases.ex
+++ lib/number_bases.ex
@@ -4,8 +4,13 @@ defmodule NumberBases do
   end
 
   defp parse(number, base) do
-    String.to_integer(number, base)
+    number
+    |> String.to_charlist()
+    |> Enum.reduce(0, fn digit, acc -> acc * base + parse_digit(digit) end)
   end
+
+  defp parse_digit(char) when char in ?A..?Z, do: char - 55
+  defp parse_digit(char) when char in ?0..?9, do: char - ?0
 
   defp format(number, base) do
     Integer.to_string(number, base)
